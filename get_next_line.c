1e502d7ce9a24d61ec0461fd66cfd52188b1e4fb
thasampa <thallessampaio89@gmail.com>
2025-12-01 18:34:15 -0300

    still

--- get_next_line.c
+++ get_next_line.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: thasampa <thasampa@student.42.rio>         +#+  +:+       +#+        */
+/*   By: thasampa <thasampa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/28 18:22:09 by thasampa          #+#    #+#             */
-/*   Updated: 2025/12/01 14:11:34 by thasampa         ###   ########.fr       */
+/*   Updated: 2025/12/01 18:32:18 by thasampa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,19 +16,24 @@ char	*get_next_line(int fd)
 {
 	static char	*stash;
 	char		*buffer;
+	int			bytesRead;
 
 	if (stash == NULL)
 		stash = ft_strdup("");
-	else
-		buffer = ft_strdup(stash);
-	while (read(fd, buffer, 1) > 0)
+	buffer = (char *)malloc((BUFFER_SIZE + 1) * sizeof(char));
+	while (!ft_strchr(stash, '\n'))
 	{
-		ft_strjoin(buffer, stash);
+		bytesRead = read(fd, buffer, BUFFER_SIZE);
+		if (bytesRead <= 0)
+			break;
+		buffer[bytesRead] = '\0';
+		stash = ft_strjoin(stash, buffer);
 	}
-	printf("%d bytes read!\n", bytesRead);
-	printf("File Contents: %s\n", buffer);
+	free(buffer);
+	return (stash);
 }
-
+#include <stdio.h>
+#include <fcntl.h>
 int	main(void)
 {
 	char	*filePath = "file";
--- get_next_line.h
+++ get_next_line.h
@@ -3,22 +3,26 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: thasampa <thasampa@student.42.rio>         +#+  +:+       +#+        */
+/*   By: thasampa <thasampa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/28 18:22:51 by thasampa          #+#    #+#             */
-/*   Updated: 2025/12/01 12:25:18 by thasampa         ###   ########.fr       */
+/*   Updated: 2025/12/01 18:04:51 by thasampa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
+# define BUFFER_SIZE 1
 # include <sys/types.h>
 # include <stdlib.h>
-# include <fcntl.h>
+# include <unistd.h>
+# include <stdint.h>
 
 size_t	ft_strlen(const char *src);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strdup(const char *s);
+void	*ft_calloc(size_t num, size_t size);
 
 #endif
