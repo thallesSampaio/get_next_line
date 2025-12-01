0564609a25d880111bcf3ef77f218bb9ee078df0
thasampa <thallessampaio8@gmail.com>
2025-12-01 14:11:58 -0300

    still learning

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: thasampa <thasampa@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/28 18:22:09 by thasampa          #+#    #+#             */
-/*   Updated: 2025/12/01 12:34:19 by thasampa         ###   ########.fr       */
+/*   Updated: 2025/12/01 14:11:34 by thasampa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,17 @@
 
 char	*get_next_line(int fd)
 {
-	char	buffer[1024];
-	int		bytesRead;
+	static char	*stash;
+	char		*buffer;
 
-	bytesRead = read(fd, buffer, 1024);
+	if (stash == NULL)
+		stash = ft_strdup("");
+	else
+		buffer = ft_strdup(stash);
+	while (read(fd, buffer, 1) > 0)
+	{
+		ft_strjoin(buffer, stash);
+	}
 	printf("%d bytes read!\n", bytesRead);
 	printf("File Contents: %s\n", buffer);
 }
@@ -27,4 +34,5 @@ int	main(void)
 	char	*filePath = "file";
 
 	int	fd = open(filePath, O_RDONLY);
+	printf("%s", get_next_line(fd));
 }
